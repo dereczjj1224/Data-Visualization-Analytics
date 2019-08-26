ceef397e58300f1e46cdd68f7c430bee3fe05340
dereczjj1224 <34227525+dereczjj1224@users.noreply.github.com>
2019-08-26 01:30:42 -0700

    Add files via upload

---
+++ polo_config_local.py
@@ -0,0 +1,3 @@
+SQLALCHEMY_DATABASE_URI = 'mysql://yuegeguest:password$5@polodb.c3ffx89lowrn.us-east-2.rds.amazonaws.com/yelp_phoenix'
+#SQLALCHEMY_DATABASE_URI = 'mysql://root:password$5@localhost/yelp_phoenix'
+SQLALCHEMY_ECHO = True  # False
